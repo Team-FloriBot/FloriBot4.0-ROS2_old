f9fdaad602bc3903a9300e74a68ae88fbd361956
jorth <jorth@stud.hs-heilbronn.de>
2026-04-10 10:11:19 +0200

    edited launch file

--- ros2_ws/src/floribot/launch/robot.launch.py
+++ ros2_ws/src/floribot/launch/robot.launch.py
@@ -14,50 +14,50 @@ def generate_launch_description():
                 get_package_share_directory('base'), 'launch'),
                 '/base_node.launch.py'])
         ),
-        """
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('floribot'), 'launch'),
-                '/own_laserscan_multi_merger.launch.py'])
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(os.getenv('FLORIBOT_PACKAGE_PATH', ''), 'launch', 'own_scanner.launch.py')
-            ),
-            launch_arguments={
-                'ParentFrame': 'axesFront',
-                'LaserFrame': 'laserFront',
-                'LaserTopic': 'scanFront',
-                'TX': '0.327',
-                'TY': '0',
-                'TZ': '0',
-                'RX': '1',
-                'RY': '0',
-                'RZ': '0',
-                'RW': '0',
-                'IPAddress': '192.168.0.52'
-            }.items()
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(os.getenv('FLORIBOT_PACKAGE_PATH', ''), 'launch', 'own_scanner.launch.py')
-            ),
-            launch_arguments={
-                'ParentFrame': 'axesRear',
-                'LaserFrame': 'laserRear',
-                'LaserTopic': 'scanRear',
-                'TX': '-0.334',
-                'TY': '0',
-                'TZ': '0',
-                'RX': '0',
-                'RY': '-1',
-                'RZ': '0',
-                'RW': '0',
-                'IPAddress': '192.168.0.51'
-            }.items()
-        ),
-        """
+#        """
+#        IncludeLaunchDescription(
+#            PythonLaunchDescriptionSource([os.path.join(
+#                get_package_share_directory('floribot'), 'launch'),
+#                '/own_laserscan_multi_merger.launch.py'])
+#        ),
+#
+#        IncludeLaunchDescription(
+#            PythonLaunchDescriptionSource(
+#                os.path.join(os.getenv('FLORIBOT_PACKAGE_PATH', ''), 'launch', 'own_scanner.launch.py')
+#            ),
+#            launch_arguments={
+#                'ParentFrame': 'axesFront',
+#                'LaserFrame': 'laserFront',
+#                'LaserTopic': 'scanFront',
+#                'TX': '0.327',
+#                'TY': '0',
+#                'TZ': '0',
+#                'RX': '1',
+#                'RY': '0',
+#                'RZ': '0',
+#                'RW': '0',
+#                'IPAddress': '192.168.0.52'
+#            }.items()
+#        ),
+#        IncludeLaunchDescription(
+#            PythonLaunchDescriptionSource(
+#                os.path.join(os.getenv('FLORIBOT_PACKAGE_PATH', ''), 'launch', 'own_scanner.launch.py')
+#            ),
+#            launch_arguments={
+#                'ParentFrame': 'axesRear',
+#                'LaserFrame': 'laserRear',
+#                'LaserTopic': 'scanRear',
+#                'TX': '-0.334',
+#                'TY': '0',
+#                'TZ': '0',
+#                'RX': '0',
+#                'RY': '-1',
+#                'RZ': '0',
+#                'RW': '0',
+#                'IPAddress': '192.168.0.51'
+#            }.items()
+#        ),
+#        """
         # Include plc_connection Launch
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
