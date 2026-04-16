146521ec44b3e8f10572d191ef933fb4a6e52e06
norikaschneider <norika.schneider@gmail.com>
2026-04-16 16:31:30 +0200

    adapted kinematics formula

--- ros2_ws/src/base/src/drives/articulated_drive.cpp
+++ ros2_ws/src/base/src/drives/articulated_drive.cpp
@@ -33,12 +33,14 @@ articulatedWheelSpeed ArticulatedDrive::inverseKinematics(geometry_msgs::msg::Tw
     targetSpeed_ = cmdVelMsg.linear.x;
     targetOmega_ = cmdVelMsg.angular.z;
 
+    // winkel muss invertiert werdem
+
     if (targetSpeed_ >= 0)
     {
         retVal.Front.leftWheel  = (1/wheelRadius_) * targetSpeed_ - (axesLength_/(2*wheelRadius_))* targetOmega_;
         retVal.Front.rightWheel = (1/wheelRadius_) * targetSpeed_ + (axesLength_/(2*wheelRadius_))* targetOmega_;
-        retVal.Rear.leftWheel = (cos(angle)/wheelRadius_ - (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((frontLength_*sin(angle) / (2*wheelRadius_))- (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
-        retVal.Rear.rightWheel= (cos(angle)/wheelRadius_ + (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((frontLength_*sin(angle) / (2*wheelRadius_))+ (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
+        retVal.Rear.leftWheel = (cos(angle)/wheelRadius_ + (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((-1* frontLength_*sin(angle) / (2*wheelRadius_))- (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
+        retVal.Rear.rightWheel= (cos(angle)/wheelRadius_ - (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((-1* frontLength_*sin(angle) / (2*wheelRadius_))+ (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
     }
 
     else
