61c47443363030f3c4a5b28ac16eeac917ec7bcc
norikaschneider <norika.schneider@gmail.com>
2026-04-17 10:56:21 +0200

    adapted for backwards driving

--- ros2_ws/src/base/src/drives/articulated_drive.cpp
+++ ros2_ws/src/base/src/drives/articulated_drive.cpp
@@ -49,6 +49,7 @@ articulatedWheelSpeed ArticulatedDrive::inverseKinematics(geometry_msgs::msg::Tw
 
     else
     {
+        targetOmega_ = -targetOmega_;
         retVal.Front.leftWheel = (cos(angle)/wheelRadius_ + (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((frontLength_*sin(angle) / (2*wheelRadius_))- (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
         retVal.Front.rightWheel= (cos(angle)/wheelRadius_ - (axesLength_ * sin(angle))/(frontLength_ * wheelRadius_)) * targetSpeed_ + ((frontLength_*sin(angle) / (2*wheelRadius_))+ (axesLength_ * cos(angle))/(2*wheelRadius_)) * targetOmega_;
         retVal.Rear.leftWheel =  (1.0/wheelRadius_) * targetSpeed_ + (axesLength_/(2*wheelRadius_))* targetOmega_;
